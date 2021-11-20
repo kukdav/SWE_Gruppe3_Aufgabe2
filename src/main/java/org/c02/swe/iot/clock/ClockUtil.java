b18fe58dee900000b4a700d43180e2faa0799ff6
prappold <philipp.rappold@edu.campus02.at>
2021-11-20 14:59:46 +0100

    epic 4 clock

--- src/main/java/org/c02/swe/iot/clock/ClockUtil.java
+++ src/main/java/org/c02/swe/iot/clock/ClockUtil.java
@@ -1,18 +1,22 @@
 package org.c02.swe.iot.clock;
 
 import org.c02.swe.iot.IButton;
+import org.c02.swe.iot.LedStatus;
 import org.c02.swe.iot.cloud.api.ParticleException;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 public class ClockUtil {
 
 
-
+    IButton btn;
 
     public ClockUtil(IButton button) {
-        IButton btn = button;
+        this.btn = button;
 
     }
 
@@ -21,12 +25,17 @@ public class ClockUtil {
         int min = date.getMinutes();
         int sec = date.getSeconds();
 
-        int hourpos;
-
+        if (hour>=12){hour-=12;}
         int minpos = min / 5;
         int secpos = sec / 5;
 
-        if (minpos == 12) {  }
+
+        List<LedStatus> statuses = new ArrayList<LedStatus>();
+        statuses.add(new LedStatus(hour, Color.red));
+        statuses.add(new LedStatus(minpos, Color.green));
+        statuses.add(new LedStatus(secpos, Color.blue));
+
+        this.btn.setLeds(statuses);
 
 
     }
