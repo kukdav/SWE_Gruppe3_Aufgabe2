f9b5bcfc1805651c99d7b36c29a88d2819c9c672
prappold <philipp.rappold@edu.campus02.at>
2021-11-20 14:48:35 +0100

    Revert "epic3 init push, button und buttontest"
    This reverts commit 69d8f3d7

--- src/test/java/org/c02/iot/ButtonTest.java
+++ src/test/java/org/c02/iot/ButtonTest.java
@@ -74,44 +74,6 @@ public class ButtonTest {
     }
 
 
-    @Test
-    public void testWrapperLedStatus() throws ParticleException {
-
-        IParticleApi api = Mockito.mock(IParticleApi.class);
-
-        Button testButton = new Button(api);
-
-        testButton.setLed(new LedStatus(1,Color.white));
-        Mockito.verify(api).callMethod("led", "01255255255");
-
-    }
-
-    @Test
-    public void testWrapperLedStatuses() throws ParticleException {
-
-        IParticleApi api = Mockito.mock(IParticleApi.class);
-
-        Button testButton = new Button(api);
-        LedStatus[] statuses = {
-            new LedStatus(1, Color.white),
-            new LedStatus(3, Color.red)
-        };
-
-
-        for (LedStatus status : statuses) {
-            testButton.setLed(status);
-
-            if(status.getColor() == Color.white) {
-                Mockito.verify(api).callMethod("led", "01255255255"); //white
-            }
-            else if(status.getColor() == Color.red) {
-                Mockito.verify(api).callMethod("led", "03255000000"); //red
-            }
-
-        }
-
-    }
-
     @Test(expected = IllegalArgumentException.class)
     public void testWrapperLedInvalid() throws ParticleException {
 
