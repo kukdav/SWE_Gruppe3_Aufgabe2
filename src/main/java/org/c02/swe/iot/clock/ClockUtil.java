247c2520588ba2e6981dd94cb64169c202686297
prappold <philipp.rappold@edu.campus02.at>
2021-11-20 14:37:28 +0100

    epic 4 clock init commit

--- src/main/java/org/c02/swe/iot/clock/ClockUtil.java
+++ src/main/java/org/c02/swe/iot/clock/ClockUtil.java
@@ -1,9 +1,7 @@
 package org.c02.swe.iot.clock;
 
 import org.c02.swe.iot.IButton;
-
 import org.c02.swe.iot.cloud.api.ParticleException;
-
 
 import java.util.Date;
 
@@ -12,16 +10,27 @@ public class ClockUtil {
 
 
 
-    public ClockUtil(IButton button) {
 
+    public ClockUtil(IButton button) {
+        IButton btn = button;
 
     }
 
     public void show(Date date) throws ParticleException {
+        int hour = date.getHours();
+        int min = date.getMinutes();
+        int sec = date.getSeconds();
 
+        int hourpos;
+
+        int minpos = min / 5;
+        int secpos = sec / 5;
+
+        if (minpos == 12) {  }
 
 
     }
+
 
 
 }
