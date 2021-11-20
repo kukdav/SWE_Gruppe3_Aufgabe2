c37a4363bfe8158fbdb383ce78fb7116fa414617
prappold <philipp.rappold@edu.campus02.at>
2021-11-20 15:22:22 +0100

    epic 4 clock, last push

---
+++ src/test/java/org/c02/iot/effect/test/DateTest.java
@@ -0,0 +1,22 @@
+package org.c02.iot.effect.test;
+
+public class DateTest {
+
+
+    // not finished in time
+
+    /*@Test
+    public void testReset() throws ParticleException {
+        //setup
+
+        IButton buttonInstance = Mockito.mock(IButton.class);
+        ClockUtil clock = new ClockUtil(buttonInstance);
+
+        clock.show(new java.util.Date());
+
+        Date date = new Date(2021,11,20,15,28,46);
+
+        clock.show(date);
+
+    }*/
+}
