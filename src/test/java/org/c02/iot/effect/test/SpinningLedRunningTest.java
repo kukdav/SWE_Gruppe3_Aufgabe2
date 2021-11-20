9e92cb80d30c6911fa6e266cad5c70a42af330ec
DESKTOP-FISKTM0\nikik <niki.kachelmaier@gmail.com>
2021-11-20 11:06:27 +0100

    Test for task SpinningLed

---
+++ src/test/java/org/c02/iot/effect/test/SpinningLedRunningTest.java
@@ -0,0 +1,75 @@
+package org.c02.iot.effect.test;
+
+import org.c02.swe.iot.IButton;
+import org.c02.swe.iot.cloud.api.ParticleException;
+import org.c02.swe.iot.effect.SpinningLedRunning;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+import java.awt.*;
+
+public class SpinningLedRunningTest {
+
+
+    @Test
+    public void testReset() throws ParticleException {
+        //setup
+        IButton buttonInstance = Mockito.mock(IButton.class);
+
+        SpinningLedRunning poc = new SpinningLedRunning(buttonInstance);
+        poc.reset();
+
+        Mockito.verify(buttonInstance).allLedsOff();
+        Mockito.verifyNoMoreInteractions(buttonInstance);
+    }
+
+    @Test
+    public void testOneTick() throws ParticleException{
+        //setup
+        IButton buttonInstance = Mockito.mock(IButton.class);
+        SpinningLedRunning poc = new SpinningLedRunning(buttonInstance);
+
+        Assert.assertTrue(poc.next());
+
+        Mockito.verify(buttonInstance).setLed(1, Color.red);
+        Mockito.verifyNoMoreInteractions(buttonInstance);
+    }
+
+
+    @Test
+    public void testSequenceTwoTicks() throws ParticleException{
+        //setup
+        IButton buttonInstance = Mockito.mock(IButton.class);
+        SpinningLedRunning poc = new SpinningLedRunning(buttonInstance);
+
+        Assert.assertTrue(poc.next());
+        Assert.assertTrue(poc.next());
+
+        InOrder inOrder = Mockito.inOrder(buttonInstance);
+
+        inOrder.verify(buttonInstance).setLed(1, Color.blue);
+        inOrder.verify(buttonInstance).setLed(2, Color.green);
+
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSequence() throws ParticleException {
+        //setup
+        IButton buttonInstance = Mockito.mock(IButton.class);
+        SpinningLedRunning poc = new SpinningLedRunning(buttonInstance);
+
+        while (poc.next()) {
+        }
+
+        InOrder inOrder = Mockito.inOrder(buttonInstance);
+        for (int count = 1; count <= 10; count++) {
+            inOrder.verify(buttonInstance).setLed(count, Color.red);
+        }
+        inOrder.verify(buttonInstance).allLedsOff();
+        inOrder.verifyNoMoreInteractions();
+    }
+
+}
